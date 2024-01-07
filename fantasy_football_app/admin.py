f2d01e1b24980e0fb0597d8314335198e479a573
imifatteh <thevariousgamer232323@yahoo.com>
2024-01-06 21:27:36 -0500

    adding captain, RosteredPlayers

--- fantasy_football_app/admin.py
+++ fantasy_football_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Player, Entry
+from .models import Player, Entry, RosteredPlayers
 
 
 class PlayerAdmin(admin.ModelAdmin):
@@ -13,3 +13,11 @@ admin.site.register(Player, PlayerAdmin)
 
 admin.site.register(Entry)
 # Register your models here.
+
+
+class RosteredPlayersAdmin(admin.ModelAdmin):
+    list_display = ('player', 'entry', 'is_captain') 
+    list_filter = ('is_captain',) 
+    search_fields = ('player__name', 'entry__name') 
+
+admin.site.register(RosteredPlayers, RosteredPlayersAdmin)
