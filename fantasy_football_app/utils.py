ff1bd99087500bc9795c8a06be2457f79f4bbac5
Ethan Katnic <ethkatnic@gmail.com>
2024-01-10 10:31:02 -0800

    Fixes roster ordering, removes unused rostered_player_id field

--- fantasy_football_app/utils.py
+++ fantasy_football_app/utils.py
@@ -1,3 +1,5 @@
+from .constants import position_order
+
 '''
 Creates and returns a dict of players mapping player instance to total score
 param: entry - entry instance
@@ -5,7 +7,10 @@ return: player_total_dict - dict mapping player instance to total score
 '''
 def create_player_totals_dict_list(entry):
     player_total_dict = {}
-    player_list = entry.players.prefetch_related('playerstats_set').all()
-    for player in player_list:
-        player_total_dict[player] = player.playerstats_set.first()
+    rostered_player_list = entry.rosteredplayers_set.prefetch_related('player__playerstats_set').all().order_by('id')
+    for order, rostered_player in enumerate(rostered_player_list):
+        player_total_dict[rostered_player.player] = {
+            "player_stats": rostered_player.player.playerstats_set.first(),
+            "position": position_order[order],
+        }
     return player_total_dict
