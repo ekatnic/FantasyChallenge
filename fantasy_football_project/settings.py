2273254f9b63f21703e0643c9e0cc5b352d5581a
Ethan Katnic <ethkatnic@gmail.com>
2024-01-13 09:04:41 -0800

    Fix static

--- fantasy_football_project/settings.py
+++ fantasy_football_project/settings.py
@@ -98,6 +98,7 @@ if IS_HEROKU_APP:
         "default": dj_database_url.config(),
     }
     DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 else:
     # When running locally in development or in CI, a sqlite database file will be used instead
     # to simplify initial setup. Longer term it's recommended to use Postgres locally too.
@@ -148,6 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Don't store the original (un-hashed filename) version of static files, to reduce slug size:
 # https://whitenoise.readthedocs.io/en/latest/django.html#WHITENOISE_KEEP_ONLY_HASHED_FILES
@@ -166,3 +168,17 @@ CSRF_TRUSTED_ORIGINS = [
     'http://playoff-showdown.com',
     'https://playoff-showdown.com',
 ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
