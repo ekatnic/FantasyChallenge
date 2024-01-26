c27f9ec5ff4551d80b9d2ffab7e60f550b0bf0d3
Ethan Katnic <ethkatnic@gmail.com>
2024-01-25 18:30:16 -0800

    Add command logging

--- fantasy_football_app/management/commands/pull_api.py
+++ fantasy_football_app/management/commands/pull_api.py
@@ -1,4 +1,5 @@
 # load_player_stats.py
+import logging
 from django.core.management.base import BaseCommand
 from api_request import TankAPIClient
 from constants import WEEK_CHOICES
@@ -13,5 +14,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         date = options['date']
         week = options['week']
+        logger.info(f'Starting to pull API data for date {date} and week {week}')
         client = TankAPIClient()
-        client.process_player_stats_for_date(date, week)
+        updated_players = client.process_player_stats_for_date(date, week)
+        cache.delete('ranked_entries_dict')
+        cache.delete('players_scoring_dict')
+        logger.info(f'Players updated: {updated_players}')
+        logger.info(f'Finished pulling API data for date {date} and week {week}')
