61bbdac3e4967ffcb18fdca14e1e33b70e761e21
ekatnic <ethkatnic@gmail.com>
2024-12-16 17:25:39 -0800

    Remove unused csv views, fix up load_players job, render all players (#37)

--- fantasy_football_app/management/commands/load_players.py
+++ fantasy_football_app/management/commands/load_players.py
@@ -1,6 +1,7 @@
 # management/commands/load_players.py
 from django.core.management.base import BaseCommand
 from fantasy_football_app.models import Player
+from django.core.cache import cache
 import csv
 
 class Command(BaseCommand):
@@ -8,6 +9,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         Player.objects.all().delete()
+        cache.delete('ranked_entries_dict')
+        cache.delete('players_scoring_dict')
         with open('data/All_Players_Test.csv', 'r') as f:
             reader = csv.reader(f)
             for row in reader:
