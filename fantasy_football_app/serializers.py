aefcf3250a55d60daff4c837cec5801342594482
anguswg-ucsb <anguswatters@gmail.com>
2024-12-22 10:58:03 -0800

    initial commit based of improve-entry-flow, added auth serializers to serializers.py

--- fantasy_football_app/serializers.py
+++ fantasy_football_app/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+
 from .models import (
     Entry,
     Player,
     RosteredPlayers,
 )
+
 from django.contrib.auth import get_user_model
 
 class EntrySerializer(serializers.ModelSerializer):
@@ -27,3 +31,84 @@ class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
         fields = '__all__'
+        
+# ---------------------------------------------------------------
+# ---- Auth serializers ----
+# ---------------------------------------------------------------
+        
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name')
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+    email = serializers.EmailField(required=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2', 'email', 'first_name', 'last_name')
+
+    def validate(self, attrs):
+        if attrs['password1'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        
+        # Force username to be email
+        attrs['username'] = attrs['email'].lower()
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['email'].lower(),
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+
+        user.set_password(validated_data['password1'])
+        user.save()
+
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+
+    def validate(self, attrs):
+        attrs['username'] = attrs['username'].lower()
+        return attrs
+
+
+
+class ForgotPasswordSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+
+    def validate_email(self, value):
+        # Convert to lowercase for consistency
+        return value.lower()
+
+
+class ConfirmForgotPasswordSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    confirmation_code = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, validators=[validate_password])
+
+    def validate_email(self, value):
+        # Convert to lowercase for consistency
+        return value.lower()
+
+class ChangePasswordSerializer(serializers.Serializer):
+    new_password = serializers.CharField(required=True, validators=[validate_password])
+    confirm_password = serializers.CharField(required=True)
+
+    def validate(self, attrs):
+        if attrs['new_password'] != attrs['confirm_password']:
+            raise serializers.ValidationError({
+                "password": "Password fields didn't match."
+            })
+        return attrs
