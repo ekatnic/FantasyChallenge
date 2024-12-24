419257dac0d61aa8480e1d6a2ef9891c974599b4
anguswg-ucsb <anguswatters@gmail.com>
2024-12-24 12:38:00 -0800

    removed empty string base url from services/api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const BASE_URL = process.env.REACT_APP_API_BASE_URL || "";
-const BASE_URL = "";
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || "";
 
 export const getEntries = async () => {
   try {
