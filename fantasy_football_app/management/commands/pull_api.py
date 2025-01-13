2b63cab65ec1426c09831c493e4b0ac0b411fe56
Ethan <ethkatnic@gmail.com>
2025-01-12 22:40:51 -0800

    Make date arg optional

--- fantasy_football_app/management/commands/pull_api.py
+++ fantasy_football_app/management/commands/pull_api.py
@@ -7,7 +7,7 @@ import pytz
 from django.core.cache import cache
 from django.core.management.base import BaseCommand
 from fantasy_football_app.tank_api.api_request import TankAPIClient
-from fantasy_football_app.constants import WEEK_CHOICES
+from fantasy_football_app.constants import WEEK_START_DATES
 from fantasy_football_app.utils import (
     get_all_entry_score_dicts, 
     get_summarized_players, 
@@ -16,28 +16,33 @@ from fantasy_football_app.utils import (
 
 logger = logging.getLogger(__name__)
 
-def get_current_week(current_date, tz):
-    if current_date < datetime.datetime(current_date.year, 1, 18, tzinfo=tz):
+def get_current_week(current_date):
+    if current_date < WEEK_START_DATES["DIV"]:
         return 'WC'
-    elif current_date < datetime.datetime(current_date.year, 1, 25, tzinfo=tz):
+    if current_date < WEEK_START_DATES["CONF"]:
         return 'DIV'
-    elif current_date < datetime.datetime(current_date.year, 2, 1, tzinfo=tz):
+    if current_date < WEEK_START_DATES["SB"]:
         return 'CONF'
-    else:
-        return 'SB'
+    return 'SB'
 
 class Command(BaseCommand):
     help = 'Loads player stats for current week'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'date', 
+            type=str,
+            nargs='?', # optional
+            help='The game date in YYYYMMDD format. Defaults to current.'
+        )
 
     def handle(self, *args, **options):
         job_enabled = os.getenv('LIVE_GAME_LOAD_ENABLED', 'false').lower() == 'true'
         if not job_enabled:
             print("Job is disabled in Heroku vars. Set LIVE_GAME_LOAD_ENABLED to 'true' to pull data. Exiting.")
             return
-        pst = pytz.timezone('America/Los_Angeles')
-        date = datetime.datetime.now(pst)
-        week = get_current_week(date, pst)
-        date = date.strftime('%Y%m%d')
+        date = options['date'] if options['date'] else datetime.datetime.now(pytz.timezone('America/Los_Angeles')).strftime('%Y%m%d')
+        week = get_current_week(date)
         logger.info(f'Starting to pull API data for date {date} and week {week}')
         client = TankAPIClient()
         updated_players = client.process_player_stats_for_date(date, week)
